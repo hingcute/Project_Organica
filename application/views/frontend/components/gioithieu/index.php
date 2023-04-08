bfde967cce32881da144b6e9295ab9293d9e96d9
Hua Thai Hung <huathaihung2001@gmail.com>
2023-04-08 14:47:22 +0700

    finish project

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,10 @@
+<center><h3>Giới thiệu về niên luận cơ sở - web thực phẩm hữu cơ<br/>
+  "Công ty CP Đầu tư Organica - Phát triển và phân phối thực phẩm hữu cơ cho người Việt" được thiết kế theo tiêu chi dễ 	<br/>
+ nhìn thân thiện với người dùng
+	<br/>
+• Được thực hiện bởi sinh viên:
+	<br/>
+
+Hứa Thái Hưng - B1909922
+	<br/>
+• Giảng viên hướng dẫn: Trần Cao Đệ</h3></center>
